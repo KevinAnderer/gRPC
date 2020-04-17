c37720deabc25d02b83cb24fa2331c400c761533
KevinAnderer <kevin.anderer@web.de>
2020-04-17 11:21:00 +0200

    test

--- setPreferredSupplierForProduct.js
+++ setPreferredSupplierForProduct.js
@@ -2,26 +2,25 @@ const client = require('./client')
 const UnknownSupplierException = require('./Error')
 const UnknownProductException = require('./Error')
 
-const pName = "Produkt"
-const sName = "SupplierA"
+const pName = "ProduktA"
+const sName = "SupplierX"
 
 const noSupplier = new UnknownSupplierException(`Es wurde kein Supplier "${sName}" gefunden`)
 const noProduct = new UnknownProductException(`Es wurde kein Produkt "${pName}" gefunden`)
 
 
-client.setPreferredSupplierForProduct({pName, sName}, (error, response) => {
+client.setPreferredSupplierForProduct({ pName, sName }, (error, response) => {
 
-    if(!error)
-       console.log(`Dem Produkt "${pName}" wurde erfolgreich der Preferred Supplier "${sName}" hinzugefügt`)
-    else if (error.details.includes('Produkt')){
-        
+    if (!error)
+        console.log(`Dem Produkt "${pName}" wurde erfolgreich der Preferred Supplier "${sName}" hinzugefügt`)
+    else if (error.details.includes('Produkt')) {
+
         console.error(error)
-        console.error(noProduct)        
-    }
-    else if (error.details.includes('Supplier')){
+        console.error(noProduct)
+    } else if (error.details.includes('Supplier')) {
 
         console.error(error)
         console.error(noSupplier)
     }
-    
+
 });
